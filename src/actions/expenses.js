ed39b1f4eb2241fd0746bf0fc3434cbf06e79b16
Rohit Raj <rohitraj21071999@gmail.com>
2021-01-22 03:33:24 +0530

    Add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -46,6 +46,14 @@ export const editExpense = (id, updates) => ({
     updates
 });
 
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        });
+    };
+};
+
 //SET-EXPENSES
 export const setExpenses = (expenses) => ({
     type: 'SET_EXPENSES',
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -4,6 +4,7 @@ import {
     startAddExpense, 
     addExpense, 
     editExpense, 
+    startEditExpense,
     removeExpense, 
     setExpenses, 
     startSetExpenses, 
@@ -54,6 +55,24 @@ test('Should setup edit action object', () => {
         updates: {
             note: 'New note value'
         }
+    });
+});
+
+test('should edit Expense from database', (done) => {
+    const store = createMockStore({});
+    const id = expenses[0].id;
+    const updates = { amount: 21045 };
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => { 
+        expect(snapshot.val().amount).toBe(updates.amount);
+        done();
     });
 });
 
